5966ab70c115a866831efc821074616cb9806de2
Marius Rodi <marius.r.rodi@gmail.com>
2018-10-15 10:52:21 +0200

    implementing working user model

--- src/main/scala/de/htwg/se/shithead/model/User.scala
+++ src/main/scala/de/htwg/se/shithead/model/User.scala
@@ -1,5 +1,8 @@
 package de.htwg.se.shithead.model
 
-case class User(uuid: String = java.util.UUID.randomUUID.toString,
-                name: String, 
-                userCardStack: List[Card])
+case class User(name: String) {
+
+    val UUID: String = java.util.UUID.randomUUID.toString
+    val NAME: String = name
+    var userCardStack: List[Card] = List()
+}
