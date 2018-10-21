404aea63e08c8d0c8f8fd5a68b26ec7b4e60d648
Marius Rodi <marius.r.rodi@gmail.com>
2018-10-21 18:39:04 +0200

    initializing Objects at start

--- src/main/scala/de/htwg/se/shithead/Shithead.scala
+++ src/main/scala/de/htwg/se/shithead/Shithead.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.shithead
 
 import de.htwg.se.shithead.model.CardStack
-import de.htwg.se.shithead.model.User
+import de.htwg.se.shithead.model.UserList
 
 import scala.io.StdIn.readLine
 
@@ -10,8 +10,9 @@ object Shithead {
     def main(args: Array[String]) {
         println("This is Shithead!")
 
-        var stack = CardStack()
-        var userList: List[User] = List()
+        var stack = CardStack
+        stack.shuffle()
+        var userList = UserList
 
         var input: String = ""
         do {
