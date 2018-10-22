58637b28616d0a8ad82353481d00edeb5986573d
Marius Rodi <marius.r.rodi@gmail.com>
2018-10-22 12:26:30 +0200

    implementing functions addUser and removeUser

--- src/main/scala/de/htwg/se/shithead/model/UserList.scala
+++ src/main/scala/de/htwg/se/shithead/model/UserList.scala
@@ -4,6 +4,24 @@ object UserList {
     var userList: List[User] = List()
 
     def addUser(name: String) = {
-        userList = new User(name) :: userList
+        if (addActionIsValid(name)) {
+            userList = new User(name) :: userList
+            true
+        } else {
+            false
+        }
     }
+
+    def removeUser(name: String) = {
+        if (removeActionIsValid(name)) {
+            userList = userList.filter(_.name == name)
+            true
+        } else {
+            false
+        }
+    }
+
+    private def removeActionIsValid(name: String) = userList.exists(_.name == name)
+
+    private def addActionIsValid(name: String) = userList.exists(_.name != name)
 }
