abd53223dbd165ad9fef55100faa4050cb6b4f5f
Julius <julius_lipp@yahoo.de>
2018-10-22 11:50:55 +0200

    some more changes

--- src/main/scala/de/htwg/se/shithead/controller/Controller.scala
+++ src/main/scala/de/htwg/se/shithead/controller/Controller.scala
@@ -1,5 +1,10 @@
 package de.htwg.se.shithead.controller
 
+import de.htwg.se.shithead.model.User
+import de.htwg.se.shithead.Shithead
+import de.htwg.se.shithead.
+
+
 object Controller {
     //zustand 
 
@@ -17,6 +22,7 @@ object Controller {
                 case "new" => println(newUser(splitted(2)))
                 case "play" => println(playCard(splitted(1) toInt))
                 case "switch" => println(switchCards(splitted(1) toInt, splitted(2) toInt))
+                case "remove" => println(removeUser())
 
             }
         } else {
@@ -27,13 +33,13 @@ object Controller {
 
     def matches(line : String): Boolean = line.matches("((\\s)*y(\\s)*)|((\\s)*n(\\s)*)|((\\s)*(new)(\\s)+user(\\s)+(\\w){2,20}(\\s)*)|" +
         "((\\s)*start(\\s)*)|((\\s)*(switch)(\\s)+[123](\\s)+[123](\\s)*)|" +
-        "((\\s)*play(\\s)+(\\d)+(\\s)*)")
+        "((\\s)*play(\\s)+(\\d)+(\\s)*)|((\\s)*remove(\\s)+(\\w){2,20}")
 
     def answerYes(): String = {
         if (zustand == 3) {
             "hallo"
         } else {
-            "Dieses Kommando ist nicht möglich"
+            "Opeartion not available"
         }
     }
 
@@ -41,15 +47,21 @@ object Controller {
         if (zustand == 2) {
             "bla"
         } else {
-            "Dieses Kommando ist nicht möglich"
+            "Opeartion not available"
         } 
     }
     
     def newUser(name : String): String = {
         if (zustand == 0) {
-            "hallo"    
+            if (Shithead userlist length  < 6) {
+                val bla = new User(name) 
+                bla :: Shithead.userlist
+                "Benutzer wurde angefügt"
+            } else {
+                "Es sind bereits zuviele Benutzer (löschen mit: remove name)"
+            }
         } else {
-            "Dieses Kommando ist nicht möglich."
+            "Opeartion not available"
         }
     }
 
@@ -57,7 +69,7 @@ object Controller {
         if (zustand == 1) {
             "hallo"
         } else {
-            "Dieses Kommando ist nicht möglich"
+            "Opeartion not available"
         }
     }
 
@@ -65,7 +77,7 @@ object Controller {
         if (zustand == 3) {
             "hallo"
         } else {
-            "Dieses Kommando ist nicht möglich"
+            "Opeartion not available"
         }
     }
 
@@ -73,7 +85,27 @@ object Controller {
         if (zustand == 4) {
             "hallo"
         } else {
-            "Dieses Kommando ist nicht möglich"
+            "Opeartion not available"
+        }
+    }
+
+    def removeUser(name : String): String = {
+        var bool: Boolean = false
+        if (zustand == 0) {
+            if (Shithead userlist length != 0) {
+                Shithead userlist foreach {
+                    if (_ equals name) {
+                        bool = true
+
+                    }
+                    if(bool) "User removed\n"
+                    else "Exsistiert nicht\n"
+                }
+            } else {
+                "No exsisting user\n"
+            }
+        } else {
+            "Opeartion not available\n"
         }
     }
 
