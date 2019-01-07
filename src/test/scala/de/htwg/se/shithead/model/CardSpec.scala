03abae278748f3fa41a75d0caa6310107a248625
Marius Rodi <marius.r.rodi@gmail.com>
2019-01-07 11:27:25 +0100

    succesfull test of class Card

--- src/test/scala/de/htwg/se/shithead/model/CardSpec.scala
+++ src/test/scala/de/htwg/se/shithead/model/CardSpec.scala
@@ -1,10 +1,15 @@
+package de.htwg.se.shithead.model
+
 import org.scalatest._
 
-class CardSpec extends WordSpec {
+class CardSpec extends WordSpec with Matchers {
     "A Card" when {
         "created with rank, suite and visibility" should {
-            "have rank not null" in {
-                assert(Card.rank.empty.size == 0)
+            val card = Card(Two, Diamond, false)
+            "have rank Two, suite Diamond and visibility false" in {
+                card.rank should be(Two)
+                card.suite should be(Diamond)
+                card.visibility should be(false)
             }
         }
     }
