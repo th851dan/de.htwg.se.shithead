9faad5dcb77162113d7a99e7e4cee471fe0724cc
Marius Rodi <marius.r.rodi@gmail.com>
2019-01-09 13:24:12 +0100

    adding test skeleton for class CardStack

---
+++ src/test/scala/de/htwg/se/shithead/model/CardStackSpec.scala
@@ -0,0 +1,11 @@
+package de.htwg.se.shithead.model
+
+import org.scalatest._
+
+class CardSpec extends WordSpec with Matchers {
+    "A CardStack" when {
+        "createt" should {
+            
+        }
+    }
+}
