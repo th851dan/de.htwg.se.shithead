35394fc671e53c792224570812194a33c28f329d
Marius Rodi <marius.r.rodi@gmail.com>
2019-01-12 13:38:34 +0100

    adding test

--- src/test/scala/de/htwg/se/shithead/model/CardSpec.scala
+++ src/test/scala/de/htwg/se/shithead/model/CardSpec.scala
@@ -2,6 +2,15 @@ package de.htwg.se.shithead.model
 
 import org.scalatest._
 
-class PlayerSpec extends WordSpec with Matchers {
-
+class CardSpec extends WordSpec with Matchers {
+    "A Card" when {
+        "created with rank, suite and visibility" should {
+            val card = Card(Two, Diamond, false)
+            "have rank Two, suite Diamond and visibility false" in {
+                card.rank should be(Two)
+                card.suite should be(Diamond)
+                card.visibility should be(false)
+            }
+        }
+    }
 }
